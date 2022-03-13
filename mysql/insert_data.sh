6ca1fc9522fae7019a6a51d3e8b43f2141bfcfe6
Your Name <you@example.com>
2022-03-13 13:49:49 +0200

    v2

--- mysql/insert_data.sh
+++ mysql/insert_data.sh
@@ -1,2 +1,5 @@
 docker cp db_backup.sql db:/tmp/db_backup.sql
+docker cp .my.cnf db:/root/.my.cnf 
+docker exec db chmod 600 /root/.my.cnf
+docker exec db mysqldump -h localhost -u root mydb < /tmp/db_backup.sql
 #execute this on docker: mysql -u root -p mydb < /tmp/db_backup.sql
